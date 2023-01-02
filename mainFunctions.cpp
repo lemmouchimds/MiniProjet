a31d9bd03afb04e1c1b269b7d18606b9ed85deb1
Lemmouchi Abderrahmane <lemmouchimds@outlook.fr>
2023-01-02 12:11:57 +0100

    Creation Function works perfectly

--- mainFunctions.cpp
+++ mainFunctions.cpp
@@ -12,6 +12,8 @@ typedef int BOOL;
 
 #define MAX_TYPES 4
 
+//typedef parcelle** carte;
+
 struct pixel
 {
 	int x;
@@ -20,8 +22,6 @@ struct pixel
 };
 
 typedef pixel parcelle;
-typedef parcelle** carte;
-
 
 struct objet 
 {
@@ -29,12 +29,64 @@ struct objet
 	objet* suiv;
 };
 
+/*
+//déclaration de la File
+typedef parcelle Telm;
+typedef struct Element* EFile;
+typedef struct Element { Telm  Val; EFile Suiv; } CelluleF;
+typedef struct { EFile Tete, Queue; } File;
+
+////primitives des Files
+//procedure Initfile
+void Initfile(File* F)
+{
+	(*F).Tete = NULL; (*F).Queue = NULL;
+}
+
+//procedure Enfiler
+void Enfiler(File* F, Telm x)
+{
+	EFile V;
+	V = (EFile)malloc(sizeof(CelluleF));
+	V->Val = x;
+	V->Suiv = NULL;
+	if ((*F).Tete == NULL) (*F).Tete = V; //la file est vide
+	else (*F).Queue->Suiv = V;
+	(*F).Queue = V;
+}
+
+//procedure Defiler
+void Defiler(File* F, Telm* x)
+{
+	EFile V;
+	V = (*F).Tete;
+	*x = V->Val; //ou  *x=(*F).Tete->Val;
+	if ((*F).Tete == (*F).Queue) { (*F).Tete = NULL; (*F).Queue = NULL; }
+	else (*F).Tete = (*F).Tete->Suiv; // ou  (*F).Tete=V->Suiv;
+	free(V);
+}
+
+//Fonction Filevide
+int Filevide(File F)
+{
+	if (F.Tete == NULL) return 1;
+	else return 0;
+}
+
+//Fonction Tetefile
+Telm Tetefile(File F)
+{
+	return F.Tete->Val;
+}
+*/
+
 struct theme
 {
-	//file de pixels
+	//File f;
+	int nature;
 };
 
-void afficheCarte(carte c, int n, int m)
+void afficheCarte(parcelle** c, int n, int m)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -48,7 +100,7 @@ void afficheCarte(carte c, int n, int m)
 	}
 }
 
-void creationCarte(carte* c, int n, int m)
+void creationCarte(parcelle** c, int n, int m)
 {
 	BOOL doubleX = FALSE;
 	BOOL doubleY = FALSE;
@@ -62,22 +114,19 @@ void creationCarte(carte* c, int n, int m)
 			{
 				if (!doubleX)
 				{
-					c[i][j] = (parcelle*)malloc(sizeof(parcelle));
-					/*if (C)
-					{
-						C->nature = 0;
-					}*/
-					//(*c[i][j]).nature = 0;//(rand() % 4) + 1;
+					
+					int random = (rand() % 4) + 1;
+					c[i][j].nature = random;
 					doubleX = TRUE;
 				}
 				else
 				{
-					c[i][j]->nature = c[i][j - 1]->nature;
+					c[i][j].nature = c[i][j - 1].nature;
 
 					doubleX = FALSE;
 				}
-					c[i][j]->x = i;
-					c[i][j]->y = j;
+					c[i][j].x = i;
+					c[i][j].y = j;
 			}
 
 			doubleY = TRUE;
@@ -87,9 +136,9 @@ void creationCarte(carte* c, int n, int m)
 
 			for (int j = 0; j < m; j++)
 			{
-				c[i][j]->x = i;
-				c[i][j]->y = j;
-				c[i][j]->nature = c[i - 1][j]->nature;
+				c[i][j].x = i;
+				c[i][j].y = j;
+				c[i][j].nature = c[i - 1][j].nature;
 			}
 			doubleY = FALSE;
 		}
@@ -97,15 +146,34 @@ void creationCarte(carte* c, int n, int m)
 	}
 }
 
-int main()
+void creationTEST(int** c, int n, int m)
 {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			c[i][j] = (rand() % 4) + 1;
+		}
+	}
+}
+
+int main()
+{	
+	srand(0);
 	int n = 10;
 	int m = 10;
 
-	carte* c = (carte*)malloc(sizeof(carte) * m * n);
-	creationCarte(c, n, m);
+	parcelle** c = (parcelle**)malloc(sizeof(parcelle*) * m);
+	
+	for (int i = 0; i < m; i++)
+	{
+		c[i] = (parcelle*)malloc(sizeof(parcelle) * n);
 
-	afficheCarte(*c, n, m);
+	}
+
+	//creationTEST(c1, n, m);
+	creationCarte(c, n, m);
+	afficheCarte(c, n, m);
 
 	return 0;
 
