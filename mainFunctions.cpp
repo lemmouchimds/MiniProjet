ba04890a60f6acdc08ad0194dff97d171a72d171
Lemmouchi Abderrahmane <lemmouchimds@outlook.fr>
2023-01-07 14:13:52 +0100

    Ajout de l'interface console et des petites optimisations dans le codes

--- mainFunctions.cpp
+++ mainFunctions.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include <time.h>
 #include<Windows.h>
+#include "mainFunctions.h"
 
 #pragma region Definition
 
@@ -12,7 +13,7 @@ typedef int BOOL;
 #define AGRICOLE 1
 #define HABITATION 2
 #define FORETS 3
-#define INDUSTRIELLE 4e
+#define INDUSTRIELLE 4
 
 #define MAX_TYPES 4
 
@@ -108,6 +109,28 @@ BOOL parcelleEqual(parcelle p1, parcelle p2)
 
 }
 
+void printNature(int nature)
+{
+	switch (nature)
+	{
+
+	case 1:
+		printf("Agricole");
+		break;
+	case 2:
+		printf("Habitation");
+		break;
+	case 3:
+		printf("Forets");
+		break;
+	case 4:
+		printf("Industialle");
+
+	default:
+		break;
+	}
+}
+
 #pragma region Carte
 
 void creationCarte(carte c, int n, int m)
@@ -125,7 +148,7 @@ void creationCarte(carte c, int n, int m)
 				if (!doubleX)
 				{
 
-					int random = (rand() % 4) + 1;
+					int random = (rand() % MAX_TYPES) + 1;
 					c[i][j].nature = random;
 					doubleX = TRUE;
 				}
@@ -158,6 +181,7 @@ void creationCarte(carte c, int n, int m)
 
 void afficheCarte(carte c, int n, int m)
 {
+
 	HANDLE  hConsole;
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -176,14 +200,15 @@ void afficheCarte(carte c, int n, int m)
 
 	}
 
-	SetConsoleTextAttribute(hConsole, 15);
+	int white = 15;
+	SetConsoleTextAttribute(hConsole, white);
 }
 
 #pragma endregion
 
 #pragma region Objet
 
-objet* extraireObjet(parcelle** c, parcelle p, int n, int m, int nature)
+objet* extraireObjet(carte c, parcelle p, int n, int m, int nature)
 {
 	if (p.x > n || p.y > m || p.nature != nature)
 	{
@@ -194,7 +219,7 @@ objet* extraireObjet(parcelle** c, parcelle p, int n, int m, int nature)
 	result->val = p;
 	result->suiv = NULL;
 
-	if (p.x + 1 < n)
+	if (p.x + 1 < n) // aller a droite
 	{
 		parcelle pDroit;
 		pDroit.nature = c[p.x + 1][p.y].nature;
@@ -208,7 +233,7 @@ objet* extraireObjet(parcelle** c, parcelle p, int n, int m, int nature)
 	{
 		temp = temp->suiv;
 	}
-	if (p.y + 1 < m)
+	if (p.y + 1 < m) // aller en bas
 	{
 		parcelle pBas;
 		pBas.nature = c[p.x][p.y + 1].nature;
@@ -227,10 +252,7 @@ BOOL existInObjet(objet* o, parcelle p)
 
 	while (temp)
 	{
-		if (temp->val.nature == p.nature &&
-			temp->val.x == p.x &&
-			temp->val.y == p.y
-			)
+		if (parcelleEqual(temp->val, p))
 		{
 			return TRUE;
 		}
@@ -276,7 +298,7 @@ carte creationCarteObjet(objet* o, int n, int m)
 
 void afficheObjet(objet o, int n, int m)
 {
-
+	printf("\n\n---Affichage d'un objet ---\n\n");
 	carte c = creationCarteObjet(&o, n, m);
 	afficheCarte(c, n, m);
 }
@@ -385,6 +407,9 @@ carte creationCarteTheme(theme t, int n, int m)
 
 void afficheTheme(theme t, int n, int m)
 {
+	printf("\n\n---Affichage de theme de la nature ");
+	printNature(t.nature);
+	printf("--- \n\n");
 	carte c = creationCarteTheme(t, n, m);
 	afficheCarte(c, n, m);
 }
@@ -416,25 +441,7 @@ void afficheStat(theme t, int count, int n, int m)
 	afficheTheme(t, n, m);
 
 	printf("Il y'a %d de parcelle de type ", count);
-	switch (t.nature)
-	{
-
-	case 1:
-		printf("Agricole");
-		break;
-	case 2:
-		printf("Habitation");
-		break;
-	case 3:
-		printf("Forets");
-		break;
-	case 4:
-		printf("Industialle");
-
-	default:
-		break;
-	}
-
+	printNature(t.nature);
 
 	float surface = n * m;
 	
@@ -445,6 +452,9 @@ void afficheStat(theme t, int count, int n, int m)
 
 void stat(carte c, int n, int m)
 {
+
+	printf("\t--- Statistique ---\n");
+
 	theme t[MAX_TYPES];
 
 	for (int i = 1; i <= MAX_TYPES; i++)
@@ -464,11 +474,90 @@ void stat(carte c, int n, int m)
 
 #pragma endregion
 
-int main()
+#pragma region Interface
+
+int choisirFonction()
 {
-	srand(time(0));
-	int n = 15;
-	int m = 15;
+	printf("\n---Vous pouver :---\n");
+	printf("1- Cree une carte.\n");
+	printf("2- Afficher la carte\n");
+	printf("3- Extraire et afficher un objet\n");
+	printf("4- Extraire et afficher un theme\n");
+	printf("5- Afficher des statistiques\n");
+	printf("0- Sortire de l'application\n");
+
+	int respone = -1;
+	while (respone < 0 || respone > 5)
+	{
+		printf("Donner le numero de la fonction : ");
+		scanf_s("%d", &respone);
+	}
+
+	return respone;
+}
+
+
+void Choix1(parcelle** c, int n, int m)
+{
+	creationCarte(c, n, m);
+	printf("Voici la carte cree :\n");
+	afficheCarte(c, n, m);
+}
+
+void Choix3(carte c, int n, int m)
+{
+	printf("Donner la position d'une parcelle :\n");
+	int x = -1;
+	while (x < 0 || x > n)
+	{
+		printf("x = ");
+		scanf_s("%d", &x);
+	}
+
+	int y = -1;
+	while (y < 0 || y > n)
+	{
+		printf("y = ");
+		scanf_s("%d", &y);
+	}
+
+	parcelle p;
+	p.x = x;
+	p.y = y;
+	p.nature = c[x][y].nature;
+	objet* o = (objet*)malloc(sizeof(objet));
+	o = extraireObjet(c, p, n, m, p.nature);
+	afficheObjet(*o, n, m);
+}
+
+void Choix4(carte c, int n, int m)
+{
+	int nature = -1;
+	while (nature < 0 || nature > MAX_TYPES)
+	{
+		printf("Donner une nature a extraire (entre 1 et %d) : ", MAX_TYPES);
+		scanf_s("%d", &nature);
+	}
+	theme* t = (theme*)malloc(sizeof(theme));
+	extraireTheme(c, t, n, m, nature);
+	afficheTheme(*t, n, m);
+}
+
+void consolInterface()
+{
+	printf("Bonjour\n");
+	int n = 0;
+	int m = 0;
+
+	while (n <= 0)
+	{
+		printf("Donner la taille de la carte : ");
+		scanf_s("%d", &n);
+	}
+
+
+	m = n;
+	int choix = 0;
 
 	parcelle** c = (parcelle**)malloc(sizeof(parcelle*) * m);
 
@@ -479,35 +568,47 @@ int main()
 	}
 
 	creationCarte(c, n, m);
-	afficheCarte(c, n, m);
+	printf("Une carte a ete generer\n\n");
 
-	printf("\n");
+	do
+	{
+		choix = choisirFonction();
+		switch (choix)
+		{
+			case 1:
+				Choix1(c, n, m);
+				break;
+			case 2:
+				afficheCarte(c, n, m);
+				break;
+			case 3:
+				Choix3(c, n, m);
+				break;
 
-	//printf("give me x and y :");
-	int x = 0, y = 0;
-	/*scanf_s("%d", &x);
-	scanf_s("%d", &y);*/
+			case 4:
+				Choix4(c, n, m);
+				break;
+			case 5:
+				stat(c, n, m);
+
+			default:
+				break;
+		}
+	} while (choix);
 
 
+	printf("---Merci !---");
 
-	parcelle p;
-	p.x = x;
-	p.y = y;
+}
 
-	p.nature = c[x][y].nature;
-
-	/*objet* o = (objet*)malloc(sizeof(objet));
-	o = extraireObjet(c, p, n, m, p.nature);
-	afficheObjet(*o, n, m);*/
-
-	/*theme* t = (theme*)malloc(sizeof(theme));
-	extraireTheme(c, t, n, m, p.nature);
-	afficheTheme(*t, n, m);*/
+#pragma endregion
 
 
-	stat(c, n, m);
+int main()
+{
+	srand(time(0));
 
-	int abc = 0;
+	consolInterface();
 	return 0;
 
 
